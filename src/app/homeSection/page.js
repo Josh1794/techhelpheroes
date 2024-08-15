c41ea9f79d2088f7f6284bde940608b2d02db2eb
Joshua Grossman <46975990+Josh1794@users.noreply.github.com>
2024-08-15 12:27:53 -0400

    Added new more performant background animation

--- src/app/homeSection/page.js
+++ src/app/homeSection/page.js
@@ -1,40 +1,22 @@
 'use client';
-
 import styles from './homeSection.module.css';
-import { useEffect, useState, useRef } from 'react';
-import NET from 'vanta/dist/vanta.net.min';
 
 export default function HomePage() {
-  // const [vantaEffect, setVantaEffect] = useState(null);
-  // const myRef = useRef(null);
-  // useEffect(() => {
-  //   if (!vantaEffect) {
-  //     setVantaEffect(
-  //       NET({
-  //         el: myRef.current,
-  //         mouseControls: true,
-  //         touchControls: true,
-  //         gyroControls: true,
-  //         // minHeight: 200.0,
-  //         // minWidth: 200.0,
-  //         scale: 1.0,
-  //         scaleMobile: 0.25,
-  //         color: 0xfc9708,
-  //         points: 11.0,
-  //         spacing: 10.0,
-  //         backgroundColor: '#275070',
-  //       })
-  //     );
-  //   }
-  //   return () => {
-  //     if (vantaEffect) {
-  //       vantaEffect.destroy();
-  //     }
-  //   };
-  // }, [vantaEffect]);
   return (
-    <div className={styles.homeSectionLayout}>
+    <div className={styles.area}>
       <h1>Tech Help Heroes</h1>
+      <ul className={styles.circles}>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+      </ul>
     </div>
   );
 }
