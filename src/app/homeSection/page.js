6443694452b31171df96a47c598eea4e59369e04
Joshua Grossman <46975990+Josh1794@users.noreply.github.com>
2024-08-15 11:41:12 -0400

    Trying to fix bug

--- src/app/homeSection/page.js
+++ src/app/homeSection/page.js
@@ -5,33 +5,35 @@ import { useEffect, useState, useRef } from 'react';
 import NET from 'vanta/dist/vanta.net.min';
 
 export default function HomePage() {
-  const [vantaEffect, setVantaEffect] = useState(null);
-  const myRef = useRef(null);
-  useEffect(() => {
-    if (!vantaEffect) {
-      setVantaEffect(
-        NET({
-          el: myRef.current,
-          mouseControls: true,
-          touchControls: true,
-          gyroControls: true,
-          // minHeight: 200.0,
-          // minWidth: 200.0,
-          scale: 1.0,
-          scaleMobile: 0.25,
-          color: 0xfc9708,
-          points: 11.0,
-          spacing: 10.0,
-          backgroundColor: '#275070',
-        })
-      );
-    }
-    return () => {
-      if (vantaEffect) vantaEffect.destroy();
-    };
-  }, [vantaEffect]);
+  // const [vantaEffect, setVantaEffect] = useState(null);
+  // const myRef = useRef(null);
+  // useEffect(() => {
+  //   if (!vantaEffect) {
+  //     setVantaEffect(
+  //       NET({
+  //         el: myRef.current,
+  //         mouseControls: true,
+  //         touchControls: true,
+  //         gyroControls: true,
+  //         // minHeight: 200.0,
+  //         // minWidth: 200.0,
+  //         scale: 1.0,
+  //         scaleMobile: 0.25,
+  //         color: 0xfc9708,
+  //         points: 11.0,
+  //         spacing: 10.0,
+  //         backgroundColor: '#275070',
+  //       })
+  //     );
+  //   }
+  //   return () => {
+  //     if (vantaEffect) {
+  //       vantaEffect.destroy();
+  //     }
+  //   };
+  // }, [vantaEffect]);
   return (
-    <div ref={myRef} className={styles.homeSectionLayout}>
+    <div className={styles.homeSectionLayout}>
       <h1>Tech Help Heroes</h1>
     </div>
   );
